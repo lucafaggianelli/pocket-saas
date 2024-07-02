3edb98c17b3f27fbaed583e5de03e98d6efd7d80
Luca Faggianelli <luca.faggianelli@gmail.com>
2024-07-02 14:26:16 +0200

    automatically set PB URL in dev and prod
    close #5

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,3 +1,8 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+// During development, the PocketBase server is running on localhost:8090
+// and the frontend is running on another port, so we need to specify the host
+// But in production, the frontend is served by PocketBase itself, so we can use '/'
+const pbHost = import.meta.env.PROD ? "/" : "http://127.0.0.1:8090";
+
+export const pb = new PocketBase(pbHost);
